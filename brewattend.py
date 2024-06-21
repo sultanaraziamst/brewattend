b70f282abeeb11f878af66a9530cb5861b5cb4a2
sultanaraziamst <sultanaraziaofficial@gmail.com>
2024-06-21 19:17:35 +0600

    addemployeeid

--- brewattend.py
+++ brewattend.py
@@ -7,4 +7,10 @@ class CoffeeShopAttendanceTracker:
 
     def add_employees(self, employee_id, employee_name):
         if employee_id not in self.employees:
+            self.employees[employee_id] = {
+                'name': employee_name,
+                'attendance':[]
+        
+            }
+            print(f"Employee '{employee_name}' added successfully.")
             
